185717f29590c3bf2e027aa4f2722361087f695b
José Javier Cordero León <javicordero20022016@gmail.com>
2018-08-23 18:42:24 -0600

    Vista create/edit de encuestadores
    Se agregó el campo para el código en las vistas para poder ingresarlo o modificarlo

--- resources/views/encuestadores/fields.blade.php
+++ resources/views/encuestadores/fields.blade.php
@@ -1,25 +1,31 @@
+<!-- Codigo del usuario Field -->
+<div class="form-group col-sm-6">
+    {!! Form::label('user_code', 'Código:') !!}
+    {!! Form::text('user_code', null, ['class' => 'form-control']) !!}
+</div>
+
 <!-- Nombre Field -->
 <div class="form-group col-sm-6">
-        {!! Form::label('name', 'Nombre:') !!}
-        {!! Form::text('name', null, ['class' => 'form-control']) !!}
-    </div>
-    
-    <!-- Email Field -->
-    <div class="form-group col-sm-6">
-        {!! Form::label('email', 'Email:') !!}
-        {!! Form::text('email', null, ['class' => 'form-control']) !!}
-    </div>
-    
-    <!-- Password Field -->
-    <div class="form-group col-sm-6">
-        {!! Form::label('password', 'Contraseña:') !!}
-        {{-- {!! Form::password('password',['class' => 'form-control', 'placeholder' => 'Password', 'type' => 'password']) !!} --}}
-        {!! Form::password('password', ['class' => 'form-control']) !!}
-    </div>
-    
-    <!-- Submit Field -->
-    <div class="form-group col-sm-12">
-        {!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
-        <a href="{!! route('encuestadores.index') !!}" class="btn btn-default">Cancelar</a>
-    </div>
+    {!! Form::label('name', 'Nombre:') !!}
+    {!! Form::text('name', null, ['class' => 'form-control']) !!}
+</div>
+
+<!-- Email Field -->
+<div class="form-group col-sm-6">
+    {!! Form::label('email', 'Email:') !!}
+    {!! Form::text('email', null, ['class' => 'form-control']) !!}
+</div>
+
+<!-- Password Field -->
+<div class="form-group col-sm-6">
+    {!! Form::label('password', 'Contraseña:') !!}
+    {{-- {!! Form::password('password',['class' => 'form-control', 'placeholder' => 'Password', 'type' => 'password']) !!} --}}
+    {!! Form::password('password', ['class' => 'form-control']) !!}
+</div>
+
+<!-- Submit Field -->
+<div class="form-group col-sm-12">
+    {!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
+    <a href="{!! route('encuestadores.index') !!}" class="btn btn-default">Cancelar</a>
+</div>
     
