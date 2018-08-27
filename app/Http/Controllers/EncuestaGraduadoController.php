45424e3d5e3f82d168ecad54f621eeabd2b730f3
José Javier Cordero León <javicordero20022016@gmail.com>
2018-08-26 21:44:44 -0600

    Controlador EncuestraGraduado
    Se agrega el metodo para poder asignar encuestas a los encuestadores

--- app/Http/Controllers/EncuestaGraduadoController.php
+++ app/Http/Controllers/EncuestaGraduadoController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\EncuestaGraduado;
+use App\User;
+use DB;
 use Flash;
 
 class EncuestaGraduadoController extends Controller
@@ -14,7 +16,70 @@ class EncuestaGraduadoController extends Controller
         return view('encuestas_graduados.index', compact('encuestas'));
     }
 
-    public function asignar($id_encuestador, $id_supervisor) {
-        return "El ID del encuestador es el ".$id_encuestador.", y el ID del supervisor es el ".$id_supervisor;
+    public function asignar($id_supervisor, $id_encuestador) {
+        $encuestasNoAsignadas = EncuestaGraduado::listaDeEncuestasSinAsignar()->get();
+
+        return view('encuestadores.tabla-encuestas-no-asignadas', 
+        compact('encuestasNoAsignadas', 'id_supervisor', 'id_encuestador'));
+    }
+
+    public function crearAsignacion($id_supervisor, $id_encuestador, Request $request) {
+        /** Se obtiene el supervisor por el ID */
+        $supervisor = User::find($id_supervisor);
+
+        /** Si el supervisor no se encuentra en la BD */
+        if(empty($supervisor)) {
+            Flash::error('El supervisor con el ID '.$id_supervisor.' no existe');
+            return redirect(route('encuestadores.index'));
+        }
+
+        /** Se obtiene el encuestador por el ID */
+        $encuestador = User::find($id_encuestador);
+
+        /** Si el encuestador no se encuentra en la BD */
+        if(empty($encuestador)) {
+            Flash::error('El encuestador con el ID '.$id_encuestador.' no existe');
+            return redirect(route('encuestadores.index'));
+        }
+
+        /** Se consulta si el estado 'NO ASIGNADA' existe en la base de datos */
+        $id_estado_sin_asignar = DB::table('tbl_estados_encuestas')->select('id')->where('estado', 'NO ASIGNADA')->first();
+
+        /** Mensaje en caso de que el estado NO ASIGNADA no exista */
+        if(is_null($id_estado_sin_asignar)) {
+            Flash::error('El estado \"NO ASIGNADA\" no existe en la base de datos, contacte al administrador para más información.');
+            return redirect(route('encuestadores.index'));
+        }
+
+        /** Se consulta si el estado 'ASIGNADA' existe en la base de datos */
+        $id_estado_asignada = DB::table('tbl_estados_encuestas')->select('id')->where('estado', 'ASIGNADA')->first();
+
+        /** Mensaje en caso de que el estado ASIGNADA no exista */
+        if(is_null($id_estado_asignada)) {
+            Flash::error('El estado \"ASIGNADA\" no existe en la base de datos, contacte al administrador para más información.');
+            return redirect(route('encuestadores.index'));
+        }
+
+        $encuestas_no_encontradas = [];
+
+        /** Se hace una busqueda de la encuesta */
+        foreach($request->encuestas as $id_graduado) {
+            $registro_encuesta = EncuestaGraduado::find($id_graduado);
+
+            if(empty($registro_encuesta)) {
+                array_push($encuestas_no_encontradas, $id_graduado);
+            }
+
+            $update = $registro_encuesta->asignarEncuesta($id_supervisor, $id_encuestador, $id_estado_sin_asignar->id, $id_estado_asignada->id);
+        }
+
+        if(sizeof($encuestas_no_encontradas) <= 0) {
+            Flash::success('Se han asignado las encuestas correctamente.');
+        }
+        else {
+            Flash::warning('Algunas encuestas no han sido asignadas: '.$encuestas_no_encontradas);
+        }
+
+        return redirect(route('encuestadores.index'));
     }
 }
