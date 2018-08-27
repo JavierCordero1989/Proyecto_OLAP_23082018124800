fa6ca8546d9f021f3910830cab28a116e31e1baa
JavierCordero1989 <javicordero20022016@gmail.com>
2018-08-27 14:35:16 -0600

    Migración bitacora de cambios
    Se hace que los campos de dato original y de dato nuevo, sean nulos

--- database/migrations/2018_08_21_182749_create_bitacora_de_cambios_table.php
+++ database/migrations/2018_08_21_182749_create_bitacora_de_cambios_table.php
@@ -18,8 +18,8 @@ class CreateBitacoraDeCambiosTable extends Migration
             $table->char('transaccion', 1);
             $table->string('tabla', 50);
             $table->integer('id_registro_afectado');
-            $table->text('dato_original');
-            $table->text('dato_nuevo');
+            $table->text('dato_original')->nullable();
+            $table->text('dato_nuevo')->nullable();
             $table->datetime('fecha_hora_transaccion');
             $table->integer('id_usuario');
             $table->timestamps();
