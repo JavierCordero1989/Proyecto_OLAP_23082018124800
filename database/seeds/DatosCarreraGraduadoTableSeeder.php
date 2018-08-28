e060e12e09b8a7b8c83d24ce0b05ebbe0545ea84
JavierCordero1989 <javicordero20022016@gmail.com>
2018-08-28 11:08:56 -0600

    Seeder DatosEncuestaGraduado
    Se agregaron los tipos para lso datos de las carreras y los datos aleatorios ahora son más precisos

--- database/seeds/DatosCarreraGraduadoTableSeeder.php
+++ database/seeds/DatosCarreraGraduadoTableSeeder.php
@@ -13,31 +13,39 @@ class DatosCarreraGraduadoTableSeeder extends Seeder
      */
     public function run()
     {
-        /** Creacion de los tipos */
-        $tipo1 = TiposDatosCarrera::create([
-            'nombre' => 'Grado'
-        ]);
+        $tipos = ['CARRERA', 'UNIVERSIDAD', 'GRADO', 'DISCIPLINA', 'AREA'];
 
-        $tipo2 = TiposDatosCarrera::create([
-            'nombre' => 'Disciplina'
-        ]);
+        foreach($tipos as $tipo) {
+            $tipo_carrera = Tiposdatoscarrera::create([
+                'nombre' => $tipo
+            ]);
+        }
 
-        $tipo3 = TiposDatosCarrera::create([
-            'nombre' => 'Área'
-        ]);
+        // /** Creacion de los tipos */
+        // $tipo1 = TiposDatosCarrera::create([
+        //     'nombre' => 'Grado'
+        // ]);
+
+        // $tipo2 = TiposDatosCarrera::create([
+        //     'nombre' => 'Disciplina'
+        // ]);
+
+        // $tipo3 = TiposDatosCarrera::create([
+        //     'nombre' => 'Área'
+        // ]);
 
         /** Selecciona el ID de los tipos */
-        $tipos = TiposDatosCarrera::pluck('id')->all();
+        $ids_tipos_datos_carrera = TiposDatosCarrera::pluck('id')->all();
 
         /** Crea el faker para generar datos de prueba */
         $faker = Faker\Factory::create('es_ES');
 
         /** Creacion de grados, areas y disciplinas */
-        for($i=0; $i<100; $i++) {
+        for($i=0; $i<200; $i++) {
             $datos = DatosCarreraGraduado::create([
-                'codigo' => $faker->numerify('CD####'),
+                'codigo' => $faker->numerify('######'),
                 'nombre' => $faker->sentence,
-                'id_tipo' => $faker->randomElement($tipos)
+                'id_tipo' => $faker->randomElement($ids_tipos_datos_carrera)
             ]);
         }
     }
