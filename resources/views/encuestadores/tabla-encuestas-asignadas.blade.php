42ac45a4cc12378c7daabb1a67082b9c66f316f4
JavierCordero1989 <javicordero20022016@gmail.com>
2018-08-28 14:39:50 -0600

    Vista encuestas asignadas
    Se crea la vista para mostrar las encuestas asignadas a cada encuestador

---
+++ resources/views/encuestadores/tabla-encuestas-asignadas.blade.php
@@ -0,0 +1,102 @@
+@extends('layouts.app')
+
+@section('title', 'Encuestas sin asignar') 
+
+@section('content')
+    {{-- {!! Form::open(['route' => ['asignar-encuestas.crear-asignacion', 'id_supervisor'=>$id_supervisor,'id_encuestador'=>$id_encuestador]]) !!} --}}
+        <section class="content-header">
+            <h1 class="pull-left">Encuestas asignadas</h1>
+            <h1 class="pull-right">
+            
+                {!! Form::submit('Asignar encuestas', ['class' => 'btn btn-primary pull-right', 'style' => 'margin-top: -10px;margin-bottom: 5px;']) !!}
+                {{-- <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('asignar-encuestas.crear-asignacion', [$id_supervisor, $id_encuestador]) !!}">Asignar</a> --}}
+                
+            </h1>
+        </section>
+        <div class="content">
+            <div class="clearfix"></div>
+            <div class="clearfix"></div>
+            <div class="box-header">
+                <div class="box-body">
+
+                    @section('css')
+                        @include('layouts.datatables_css')
+                    @endsection
+
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <th>Identificacion</th>
+                                <th>Token</th>
+                                <th>Nombre</th>
+                                <th>Año de graduación</th>
+                                <th>Link para encuesta</th>
+                                <th>Sexo</th>
+                                <th>Carrera</th>
+                                <th>Universidad</th>
+                                <th>Grado</th>
+                                <th>Disciplina</th>
+                                <th>Área</th>
+                                <th>Info de contacto</th>
+                                <th>Tipo de caso</th>
+                            </thead>
+                            <tbody>
+                            @foreach($listaDeEncuestas as $encuesta)
+                                <tr>
+                                    <td>{!! Form::checkbox('encuestas[]', $encuesta->id) !!} {!! $encuesta->identificacion_graduado !!}</td>
+                                    <td>{!! $encuesta->token !!}</td>
+                                    <td>{!! $encuesta->nombre_completo !!}</td>
+                                    <td>{!! $encuesta->annio_graduacion !!}</td>
+                                    <td>
+                                        <div class="dropdown">
+                                            <button class="btn btn-default dropdown-toggle" type="button" id="dropdownEnlaceEncuesta" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                                                {{-- <i class="glyphicon glyphicon-eye-open"></i> --}}
+                                                <i class="fas fa-link"></i>
+                                                <span class="caret"></span>
+                                            </button>
+                                            <ul class="dropdown-menu" aria-labelledby="dropdownEnlaceEncuesta">
+                                                <li>
+                                                    <a href="{!! $encuesta->link_encuesta !!}" target="_blank"><i class="fas fa-eye"></i> {!! $encuesta->link_encuesta !!} </a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </td>
+                                    <td>{!! $encuesta->sexo !!}</td>
+                                    <td>{!! $encuesta->carrera !!}</td>
+                                    <td>{!! $encuesta->universidad !!}</td>
+                                    <td>{!! $encuesta->Grado !!}</td>
+                                    <td>{!! $encuesta->Disciplina !!}</td>
+                                    <td>{!! $encuesta->Area !!}</td>
+                                    <td>
+                                        {{-- Dropdown menu para mostrar la informacion de contacto del usuario --}}
+                                        <div class="dropdown">
+                                            <button class="btn btn-default dropdown-toggle" type="button" id="dropdownEnlacesInfoContacto" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                                                {{-- <i class="glyphicon glyphicon-eye-open"></i> --}}
+                                                <i class="fas fa-address-card"></i>
+                                                <span class="caret"></span>
+                                            </button>
+                                            <ul class="dropdown-menu" aria-labelledby="dropdownEnlacesInfoContacto">
+                                                <li>
+                                                    <a href="#"><i class="fas fa-eye"></i> Enlace 1</a>
+                                                </li>
+                                                <li>
+                                                    <a href="#"><i class="fas fa-eye"></i> Enlace 2</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </td>
+                                    <td>{!! $encuesta->tipo_de_caso !!}</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    {{-- {!! Form::close() !!} --}}
+@endsection
+
+@section('scripts')
+    @include('layouts.datatables_js')
+@endsection
