8f52f8e5053f403e7580d1106f218e1764567aa3
José Javier Cordero León <javicordero20022016@gmail.com>
2018-08-29 17:43:23 -0600

    Modelo Asignacion
    Se crea el modelo Asignacion para poder establecer relaciones entre usuarios y asignaciones, esto para procesos como la eliminacion de usuarios.

---
+++ app/Asignacion.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\EncuestaGraduado;
+
+class Asignacion extends Model
+{
+    protected $table = 'tbl_asignaciones';
+
+    protected $fillable = [
+        'id_graduado',
+        'id_encuestador',
+        'id_supervisor',
+        'id_estado'
+    ];
+
+    public function encuestadores() {
+        return $this->hasOne(User::class, 'id', 'id_encuestador');
+    }
+
+    public function supervisores() {
+        return $this->hasOne(User::class, 'id', 'id_supervisor');
+    }
+}
