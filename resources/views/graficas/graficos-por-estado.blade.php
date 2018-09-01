05ccf9a25815e469dc38e1887349573e94303b6d
José Javier Cordero León <javicordero20022016@gmail.com>
2018-08-31 19:28:41 -0600

    Vista de gráficos
    Cambio menor

--- resources/views/graficas/graficos-por-estado.blade.php
+++ resources/views/graficas/graficos-por-estado.blade.php
@@ -175,7 +175,7 @@
 
                 <div class="box box-info">
                     <div class="box-header with-border">
-                        <h3 class="box-title">Gráfico de pie</h3>
+                        <h3 class="box-title">Gráfico de barras combinadas</h3>
 
                         <div class="box-tools pull-right">
                             <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus" title='Minimizar'></i>
