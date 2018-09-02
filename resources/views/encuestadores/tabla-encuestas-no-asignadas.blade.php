83db567638468f72b91ee4824bf2252f493585f1
José Javier Cordero León <javicordero20022016@gmail.com>
2018-09-02 00:14:31 -0600

    Vista tabla encuestas no asignadas
    Cambios menores

--- resources/views/encuestadores/tabla-encuestas-no-asignadas.blade.php
+++ resources/views/encuestadores/tabla-encuestas-no-asignadas.blade.php
@@ -18,11 +18,6 @@
             <div class="clearfix"></div>
             <div class="box-header">
                 <div class="box-body">
-
-                    @section('css')
-                        @include('layouts.datatables_css')
-                    @endsection
-
                     <div class="table-responsive">
                         <table class="table">
                             <thead>
@@ -41,63 +36,62 @@
                                 <th>Tipo de caso</th>
                             </thead>
                             <tbody>
-                            @foreach($encuestasNoAsignadas as $encuesta)
-                                <tr>
-                                    <td>{!! Form::checkbox('encuestas[]', $encuesta->id) !!} {!! $encuesta->identificacion_graduado !!}</td>
-                                    <td>{!! $encuesta->token !!}</td>
-                                    <td>{!! $encuesta->nombre_completo !!}</td>
-                                    <td>{!! $encuesta->annio_graduacion !!}</td>
-                                    <td>
-                                        <div class="dropdown">
-                                            <button class="btn btn-default dropdown-toggle" type="button" id="dropdownEnlaceEncuesta" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                                                {{-- <i class="glyphicon glyphicon-eye-open"></i> --}}
-                                                <i class="fas fa-link"></i>
-                                                <span class="caret"></span>
-                                            </button>
-                                            <ul class="dropdown-menu" aria-labelledby="dropdownEnlaceEncuesta">
-                                                <li>
-                                                    <a href="{!! $encuesta->link_encuesta !!}" target="_blank"><i class="fas fa-eye"></i> {!! $encuesta->link_encuesta !!} </a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </td>
-                                    <td>{!! $encuesta->sexo !!}</td>
-                                    <td>{!! $encuesta->carrera !!}</td>
-                                    <td>{!! $encuesta->universidad !!}</td>
-                                    <td>{!! $encuesta->Grado !!}</td>
-                                    <td>{!! $encuesta->Disciplina !!}</td>
-                                    <td>{!! $encuesta->Area !!}</td>
-                                    <td>
-                                        <!-- Se valida que haya registros de contacto -->
-                                        @if(sizeof($encuesta->contactos) <= 0)
-                                            <a href="#modal-warning" data-toggle="modal">Agregar</a>
-                                            @include('modals.modal_agregar_info_contacto_encuesta')
-                                        @else
-                                            <!-- Dropdown menu para mostrar la informacion de contacto del usuario -->
+                                @foreach($encuestasNoAsignadas as $encuesta)
+                                    <tr>
+                                        <td>{!! Form::checkbox('encuestas[]', $encuesta->id) !!} {!! $encuesta->identificacion_graduado !!}</td>
+                                        <td>{!! $encuesta->token !!}</td>
+                                        <td>{!! $encuesta->nombre_completo !!}</td>
+                                        <td>{!! $encuesta->annio_graduacion !!}</td>
+                                        <td>
                                             <div class="dropdown">
-                                                <button class="btn btn-default dropdown-toggle" type="button" id="dropdownEnlacesInfoContacto" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                                                    <i class="fas fa-address-card"></i>
+                                            <button class="btn btn-default dropdown-toggle" type="button" id="dropdownEnlaceEncuesta-{!! $encuesta->token!!}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                                                    {{-- <i class="glyphicon glyphicon-eye-open"></i> --}}
+                                                    <i class="fas fa-link"></i>
                                                     <span class="caret"></span>
                                                 </button>
-                                                    <ul class="dropdown-menu" aria-labelledby="dropdownEnlacesInfoContacto">
-                                                        <!-- Se agrega un boton por cada registro de contacto que tenga cada encuesta, mediante un foreach -->
-                                                        @foreach($encuesta->contactos as $contacto)
-                                                            <li>
-                                                                <a href="#modal-{!! $contacto->id !!}" data-toggle="modal" ><i class="fas fa-eye"></i>{!! $contacto->nombre_referencia !!}</a>
-                                                            </li>
-                                                        @endforeach
-                                                    </ul>
-                    
-                                                    <!-- Se agregan los modales mediante un foreach -->
-                                                    @foreach($encuesta->contactos as $contacto) 
-                                                        @include('modals.modal_info_contacto')
-                                                    @endforeach
+                                                <ul class="dropdown-menu" aria-labelledby="dropdownEnlaceEncuesta">
+                                                    <li>
+                                                        <a href="{!! $encuesta->link_encuesta !!}" target="_blank"><i class="fas fa-eye"></i> {!! $encuesta->link_encuesta !!} </a>
+                                                    </li>
+                                                </ul>
                                             </div>
-                                        @endif
                                         </td>
-                                    <td>{!! $encuesta->tipo_de_caso !!}</td>
-                                </tr>
-                            @endforeach
+                                        <td>{!! $encuesta->sexo !!}</td>
+                                        <td>{!! $encuesta->Carrera !!}</td>
+                                        <td>{!! $encuesta->Universidad !!}</td>
+                                        <td>{!! $encuesta->Grado !!}</td>
+                                        <td>{!! $encuesta->Disciplina !!}</td>
+                                        <td>{!! $encuesta->Area !!}</td>
+                                        <td>
+                                            <!-- Se valida que haya registros de contacto -->
+                                            @if(sizeof($encuesta->contactos) <= 0)
+                                                <a href="#">Agregar</a>
+                                            @else
+                                                <!-- Dropdown menu para mostrar la informacion de contacto del usuario -->
+                                                <div class="dropdown">
+                                                    <button class="btn btn-default dropdown-toggle" type="button" id="dropdownEnlacesInfoContacto-{!! $encuesta->token !!}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                                                        <i class="fas fa-address-card"></i>
+                                                        <span class="caret"></span>
+                                                    </button>
+                                                        <ul class="dropdown-menu" aria-labelledby="dropdownEnlacesInfoContacto">
+                                                            <!-- Se agrega un boton por cada registro de contacto que tenga cada encuesta, mediante un foreach -->
+                                                            @foreach($encuesta->contactos as $contacto)
+                                                                <li>
+                                                                    <a href="#modal-{!! $contacto->id !!}" data-toggle="modal" ><i class="fas fa-eye"></i>{!! $contacto->nombre_referencia !!}</a>
+                                                                </li>
+                                                            @endforeach
+                                                        </ul>
+                        
+                                                        <!-- Se agregan los modales mediante un foreach -->
+                                                        @foreach($encuesta->contactos as $contacto) 
+                                                            @include('modals.modal_info_contacto')
+                                                        @endforeach
+                                                </div>
+                                            @endif
+                                        </td>
+                                        <td>{!! $encuesta->tipo_de_caso !!}</td>
+                                    </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
@@ -105,8 +99,4 @@
             </div>
         </div>
     {!! Form::close() !!}
-@endsection
-
-@section('scripts')
-    @include('layouts.datatables_js')
 @endsection
