74a4c5f31e098f1cfd0d3a7d7be1aeffab4b0bec
José Javier Cordero León <javicordero20022016@gmail.com>
2018-09-02 00:14:52 -0600

    Modal para info de contacto
    Cambios menores

--- resources/views/modals/modal_agregar_info_contacto_encuesta.blade.php
+++ resources/views/modals/modal_agregar_info_contacto_encuesta.blade.php
@@ -19,7 +19,7 @@
     <!-- /.modal-dialog -->
 </div> --}}
 
-<div class="modal modal-warning fade" id="modal-warning">
+<div class="modal modal-warning fade" id="modal-add_contact-info-{!! $encuesta->token !!}">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
@@ -33,8 +33,8 @@
                         <div class="col-sm-8 col-sm-offset-2">
                             <!-- Formulario obtenido de: -->
                             <!-- https://bootsnipp.com/snippets/featured/change-password-form-with-validation -->
-                            <form method="post" id="passwordForm">
-                                <input type="password" class="input-lg form-control" name="password1" id="password1" placeholder="Nueva contraseña" autocomplete="off">
+                            <form method="post" id="passwordForm-{!! $encuesta->token !!}">
+                                <input type="password" class="input-lg form-control" name="password1" id="password1-{!! $encuesta->token !!}" placeholder="Nueva contraseña" autocomplete="off">
                                 <div class="row">
                                     <div class="col-sm-6">
                                         <span id="8char" class="glyphicon glyphicon-remove" style="color:#FF0004;"></span> 8 Caracteres<br>
@@ -45,7 +45,7 @@
                                         <span id="num" class="glyphicon glyphicon-remove" style="color:#FF0004;"></span> Un número
                                     </div>
                                 </div>
-                                <input type="password" class="input-lg form-control" name="password2" id="password2" placeholder="Repita contraseña" autocomplete="off">
+                                <input type="password" class="input-lg form-control" name="password2" id="password2-{!! $encuesta->token !!}" placeholder="Repita contraseña" autocomplete="off">
                                 <div class="row">
                                     <div class="col-sm-12">
                                         <span id="pwmatch" class="glyphicon glyphicon-remove" style="color:#FF0004;"></span> Coincidencia de contraseñas
@@ -66,63 +66,3 @@
     </div>
     <!-- /.modal-dialog -->
 </div>
-
-@section('scripts') 
-    <script>
-        $("input[type=password]").keyup(function(){
-        var ucase = new RegExp("[A-Z]+");
-        var lcase = new RegExp("[a-z]+");
-        var num = new RegExp("[0-9]+");
-        
-        if($("#password1").val().length >= 8){
-            $("#8char").removeClass("glyphicon-remove");
-            $("#8char").addClass("glyphicon-ok");
-            $("#8char").css("color","#00A41E");
-        }else{
-            $("#8char").removeClass("glyphicon-ok");
-            $("#8char").addClass("glyphicon-remove");
-            $("#8char").css("color","#FF0004");
-        }
-        
-        if(ucase.test($("#password1").val())){
-            $("#ucase").removeClass("glyphicon-remove");
-            $("#ucase").addClass("glyphicon-ok");
-            $("#ucase").css("color","#00A41E");
-        }else{
-            $("#ucase").removeClass("glyphicon-ok");
-            $("#ucase").addClass("glyphicon-remove");
-            $("#ucase").css("color","#FF0004");
-        }
-        
-        if(lcase.test($("#password1").val())){
-            $("#lcase").removeClass("glyphicon-remove");
-            $("#lcase").addClass("glyphicon-ok");
-            $("#lcase").css("color","#00A41E");
-        }else{
-            $("#lcase").removeClass("glyphicon-ok");
-            $("#lcase").addClass("glyphicon-remove");
-            $("#lcase").css("color","#FF0004");
-        }
-        
-        if(num.test($("#password1").val())){
-            $("#num").removeClass("glyphicon-remove");
-            $("#num").addClass("glyphicon-ok");
-            $("#num").css("color","#00A41E");
-        }else{
-            $("#num").removeClass("glyphicon-ok");
-            $("#num").addClass("glyphicon-remove");
-            $("#num").css("color","#FF0004");
-        }
-        
-        if($("#password1").val() == $("#password2").val()){
-            $("#pwmatch").removeClass("glyphicon-remove");
-            $("#pwmatch").addClass("glyphicon-ok");
-            $("#pwmatch").css("color","#00A41E");
-        }else{
-            $("#pwmatch").removeClass("glyphicon-ok");
-            $("#pwmatch").addClass("glyphicon-remove");
-            $("#pwmatch").css("color","#FF0004");
-        }
-    });
-    </script>
-@endsection
