5c741954cca6355786b9af7a7af19a80fbb1b6b6
José Javier Cordero León <javicordero20022016@gmail.com>
2018-09-03 21:04:46 -0600

    Modal para info de contacto
    Se modifica para que la informacion pueda ser editada

--- resources/views/modals/modal_info_contacto.blade.php
+++ resources/views/modals/modal_info_contacto.blade.php
@@ -1,20 +1,27 @@
 <div class="modal fade" id="modal-{!! $contacto->id !!}">
     <div class="modal-dialog">
         <div class="modal-content">
+            
+            <!-- Encabezado del modal -->
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">Default Modal</h4>
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <h4 class="modal-title">Información del contacto</h4>
             </div>
+
+            <!-- Cuerpo del modal -->
             <div class="modal-body">
                 <p>{!! $contacto->identificacion_referencia !!}&hellip;</p>
                 <p>{!! $contacto->nombre_referencia !!}&hellip;</p>
                 <p>{!! $contacto->informacion_contacto !!}&hellip;</p>
                 <p>{!! $contacto->observacion_contacto !!}&hellip;</p>
             </div>
+
+            <!-- Pie del modal -->
             <div class="modal-footer">
-                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Cerrar</button>
+                <a href="{!! route('encuestadores.modificar-contacto', Crypt::encrypt($contacto->id)) !!}" class="btn btn-primary pull-right">Editar información</a>
             </div>
         </div>
     <!-- /.modal-content -->
