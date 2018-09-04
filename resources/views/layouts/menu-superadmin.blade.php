8b408eec1ed5afec27d79bb406e159b48c49ba3c
JavierCordero1989 <javicordero20022016@gmail.com>
2018-09-04 15:35:17 -0600

    Menu super admin
    Agregados los mantenimientos

--- resources/views/layouts/menu-superadmin.blade.php
+++ resources/views/layouts/menu-superadmin.blade.php
@@ -17,3 +17,35 @@
 <li class="">
     <a href="{!! route('graficos.graficos-por-estado') !!}"><i class="fas fa-chart-area"></i><span> Gráficos por estado</span></a>
 </li>
+
+<!-- Menú desplegable para datos de carrera-->
+<li class="treeview">
+    <a href="#">
+        <i class="fas fa-database"></i>
+        <span>Datos varios</span>
+        <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+        </span>
+    </a>
+    <ul class="treeview-menu">
+        <li class="{{ Request::is('carreras*') ? 'active' : '' }}">
+            <a href="{!! route('carreras.index') !!}"><i class="fa fa-graduation-cap" aria-hidden="true"></i><span>Carreras</span></a>
+        </li>
+
+        <li class="{{ Request::is('universidades*') ? 'active' : '' }}">
+            <a href="{!! route('universidades.index') !!}"><i class="fa fa-graduation-cap" aria-hidden="true"></i><span>Universidades</span></a>
+        </li>
+
+        <li class="{{ Request::is('grados*') ? 'active' : '' }}">
+            <a href="{!! route('grados.index') !!}"><i class="fa fa-graduation-cap" aria-hidden="true"></i><span>Grados</span></a>
+        </li>
+
+        <li class="{{ Request::is('disciplinas*') ? 'active' : '' }}">
+            <a href="{!! route('disciplinas.index') !!}"><i class="fa fa-graduation-cap" aria-hidden="true"></i><span>Disciplinas</span></a>
+        </li>
+
+        <li class="#">
+            <a href="#"><i class="fa fa-graduation-cap" aria-hidden="true"></i><span>Áreas</span></a>
+        </li>
+    </ul>
+</li>
