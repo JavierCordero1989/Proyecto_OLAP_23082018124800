d78a78cba0ba51fb4cb061bad7486d8293a6ac31
JavierCordero1989 <javicordero20022016@gmail.com>
2018-09-05 08:36:49 -0600

    Vistas create/edit
    Mejoras visuales

--- resources/views/carreras/fields.blade.php
+++ resources/views/carreras/fields.blade.php
@@ -1,17 +1,17 @@
 <!-- Código Field -->
-<div class="form-group col-sm-6">
+<div class="form-group col-sm-6 col-sm-offset-3">
     {!! Form::label('codigo', 'Código:') !!}
     {!! Form::text('codigo', null, ['class' => 'form-control']) !!}
 </div>
 
 <!-- Nombre Field -->
-<div class="form-group col-sm-6">
+<div class="form-group col-sm-6 col-sm-offset-3">
     {!! Form::label('nombre', 'Nombre:') !!}
     {!! Form::text('nombre', null, ['class' => 'form-control']) !!}
 </div>
 
 <!-- Submit Field -->
-<div class="form-group col-sm-12">
-    {!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
-    <a href="{!! route('carreras.index') !!}" class="btn btn-default">Cancelar</a>
+<div class="form-group col-sm-6 col-sm-offset-3">
+    {!! Form::submit('Guardar', ['class' => 'btn btn-primary col-sm-4']) !!}
+    <a href="{!! route('carreras.index') !!}" class="btn btn-default col-sm-4 col-sm-offset-4">Cancelar</a>
 </div>
