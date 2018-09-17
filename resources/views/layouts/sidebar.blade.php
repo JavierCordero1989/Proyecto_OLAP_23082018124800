4a7ce4c3c814b5a93126d871f889b28a60843324
José Javier Cordero León <javicordero20022016@gmail.com>
2018-09-16 19:58:34 -0600

    Cambio al sidebar
    Se quita el nombre del usuario y el estado "En Línea"

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -4,7 +4,7 @@
     <section class="sidebar">
 
         <!-- Sidebar user panel (optional) -->
-        <div class="user-panel">
+        {{-- <div class="user-panel">
             <div class="pull-left image">
                 <img src="{{ asset(config('global.img_app')) }}" class="img-circle"
                      alt="User Image"/>
@@ -18,7 +18,7 @@
                 <!-- Status -->
                 <a href="#"><i class="fa fa-circle text-success"></i> En línea</a>
             </div>
-        </div>
+        </div> --}}
 
         <!-- search form (Optional) -->
         {{-- <form action="#" method="get" class="sidebar-form">
