cfdafaef6fc437b4fb89e2ef6022cd37bc06225d
JavierCordero1989 <javicordero20022016@gmail.com>
2018-10-01 15:23:34 -0600

    Cambios mínimos

--- app/Http/Controllers/ReportesController.php
+++ app/Http/Controllers/ReportesController.php
@@ -8,6 +8,7 @@ use App\Disciplina;
 use DB;
 use App\Universidad;
 use Illuminate\Support\Collection;
+use App\EncuestaGraduado as Entrevista;
 
 class ReportesController extends Controller
 {
@@ -27,7 +28,52 @@ class ReportesController extends Controller
         $universidades = $request->universidades;
         $disciplinas = $request->disciplinas;
 
+        // Se seleccionó todos los sectores
+        if($sectores[0] == "1") {
+            $encuestas = Entrevista::whereIn('codigo_sector', [1,2]);
+            
+        }
+        else {
+            //Sector público
+            if($sectores[0] == "2") {
+                $encuestas = Entrevista::where('codigo_sector', 1);
+            }
+            //Sector privado
+            if($sectores[0] == "3") {
+                $encuestas = Entrevista::where('codigo_sector', 2);
+            }
+        }
+
+        // Filtra por universidades seleccionadas
+        $encuestas = Entrevista::whereIn('codigo_universidad', $universidades);
+
+        $ids_disciplinas = [];
+
+        foreach ($disciplinas as $disciplina) {
+            $area = Area::buscarPorDescriptivo($disciplina)->first();
+
+            if(empty($area)) {
+                $objeto_disciplina = Disciplina::buscarPorCodigo($disciplina)->first();
+                if(!in_array($objeto_disciplina->id, $ids_disciplinas)) {
+                    $ids_disciplinas[] = $objeto_disciplina->id;
+                }
+            }
+            else {
+                $encontrados = $area->disciplinas->pluck('id')->all();
+
+                foreach($encontrados as $dato) {
+                    $ids_disciplinas[] = $dato;
+                }
+            }
+        }
+
+        $encuestas = Entrevista::whereIn('codigo_disciplina', $ids_disciplinas)->get();
+        
+        // dd('Disciplinas', $ids_disciplinas);
+        dd('Encuestas', $encuestas);
         dd($sectores, $universidades, $disciplinas);
+
+        return view('reportes.reporte_filtrado');
     }
 
     public function traer_universidades_por_sector(Request $request) {
@@ -75,5 +121,27 @@ class ReportesController extends Controller
 
             return response()->json($datos_respuesta);
         }
+    }
+
+    private function etiquetasAreas($areas) {
+        $etiquetas = [];
+
+        foreach ($areas as $cod_area) {
+            $obj_area = Area::find($cod_area);
+            $etiquetas[] = $obj_area->descriptivo;
+        }
+
+        return $etiquetas;
+    }
+
+    private function etiquetasDisciplinas($disciplinas) {
+        $etiquetas = [];
+
+        foreach ($disciplinas as $cod_disciplina) {
+            $obj_disciplina = Disciplina::buscarPorCodigo($cod_disciplina)->first();
+            $etiquetas[] = $obj_disciplina->descriptivo;
+        }
+
+        return $etiquetas;
     }
 }
---
+++ resources/views/reportes/reporte_filtrado.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Reporte filtrado')
+
+@section('content')
+    
+@endsection
