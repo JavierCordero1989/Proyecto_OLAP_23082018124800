218351e82ab27c3e1bf325eaef09d63e4b4298da
JavierCordero1989 <javicordero20022016@gmail.com>
2018-10-16 14:56:38 -0600

    vista encuestadores/fields-edit
    Se cambia la forma en que se muestra el formulario

--- resources/views/encuestadores/fields-edit.blade.php
+++ resources/views/encuestadores/fields-edit.blade.php
@@ -1,30 +1,31 @@
-<!-- Codigo del usuario Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('user_code', 'Código:') !!}
-    {!! Form::text('user_code', null, ['class' => 'form-control']) !!}
-</div>
+<div class="col-xs-6 col-xs-offset-3">
+    <!-- Codigo del usuario Field -->
+    <div class="form-group">
+        {!! Form::label('user_code', 'Código:') !!}
+        {!! Form::text('user_code', null, ['class' => 'form-control', 'readonly'=>'readonly']) !!}
+    </div>
 
-<!-- Nombre Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('name', 'Nombre:') !!}
-    {!! Form::text('name', null, ['class' => 'form-control']) !!}
-</div>
+    <!-- Nombre Field -->
+    <div class="form-group">
+        {!! Form::label('name', 'Nombre:') !!}
+        {!! Form::text('name', null, ['class' => 'form-control']) !!}
+    </div>
 
-<!-- Email Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('email', 'Email:') !!}
-    {!! Form::text('email', null, ['class' => 'form-control']) !!}
-</div>
+    <!-- Email Field -->
+    <div class="form-group">
+        {!! Form::label('email', 'Email:') !!}
+        {!! Form::text('email', null, ['class' => 'form-control', 'readonly'=>'readonly']) !!}
+    </div>
 
-<!-- Password Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('password', 'Contraseña:') !!}
-    {!! Form::password('password', ['class' => 'form-control']) !!}
-</div>
+    <!-- Password Field -->
+    <div class="form-group">
+        {!! Form::label('password', 'Contraseña:') !!}
+        {!! Form::password('password', ['class' => 'form-control']) !!}
+    </div>
 
-<!-- Submit Field -->
-<div class="form-group col-sm-12">
-    {!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
-    <a href="{!! route('encuestadores.index') !!}" class="btn btn-default">Cancelar</a>
+    <!-- Submit Field -->
+    <div class="form-group">
+        {!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
+        <a href="{!! route('encuestadores.index') !!}" class="btn btn-default">Cancelar</a>
+    </div>
 </div>
-    
