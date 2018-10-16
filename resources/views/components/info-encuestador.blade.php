03cf8286b0f6bf086da1d6b9a0502d4e0f53bf62
JavierCordero1989 <javicordero20022016@gmail.com>
2018-10-16 14:55:16 -0600

    Componente info-encuestador
    Se crea un componente para cargar la información de los usuarios en un modal.

---
+++ resources/views/components/info-encuestador.blade.php
@@ -0,0 +1,44 @@
+<a href="#{!! $id_modal !!}" data-toggle="modal" class='btn btn-default btn-xs'>
+    <i class="glyphicon glyphicon-eye-open"></i>
+</a>
+
+<div class="modal fade" id="{{ $id_modal }}">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <h4 class="modal-title">{{ $nombre_encuestador }}</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="row" style="padding-left: 20px">
+                        <div class="col-xs-12">
+                            <!-- User code Field -->
+                            <div class="form-group">
+                                {!! Form::label('user_code', 'Código:') !!}
+                                <p>{!! $codigo_usuario !!}</p>
+                            </div>
+                            
+                            <!-- Nombre Field -->
+                            <div class="form-group">
+                                {!! Form::label('email', 'Email:') !!}
+                                <p>{!! $email !!}</p>
+                            </div>
+
+                            <div class="form-group">
+                                {!! Form::label('created_at', 'Creado el: ') !!}
+                                <p>{!! $created_at !!}</p>
+                            </div>
+                            <div class="form-group">
+                                {!! Form::label('updated_at', 'Modificado el: ') !!}
+                                <p>{!! $updated_at !!}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
