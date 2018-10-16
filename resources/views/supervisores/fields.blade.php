e6329dccf4135eac69b693406b7f72b0584994e1
JavierCordero1989 <javicordero20022016@gmail.com>
2018-10-16 15:02:32 -0600

    vista supervisores/fields
    Se modifica la vista del formulario para mostrar.

--- resources/views/supervisores/fields.blade.php
+++ resources/views/supervisores/fields.blade.php
@@ -1,31 +1,37 @@
-<!-- Nombre Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('user_code', 'Código de usuario:') !!}
-    {!! Form::text('user_code', null, ['class' => 'form-control', 'disabled' => true]) !!}
-</div>
+<div class="col-xs-6 col-xs-offset-3">
+    <!-- Nombre Field -->
+    <div class="form-group">
+        {!! Form::label('user_code', 'Código de usuario:') !!}
+        {!! Form::text('user_code', null, ['class' => 'form-control', 'required' => 'required']) !!}
+    </div>
 
-<!-- Nombre Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('name', 'Nombre:') !!}
-    {!! Form::text('name', null, ['class' => 'form-control']) !!}
-</div>
+    <!-- Nombre Field -->
+    <div class="form-group">
+        {!! Form::label('name', 'Nombre:') !!}
+        {!! Form::text('name', null, ['class' => 'form-control', 'required' => 'required']) !!}
+    </div>
 
-<!-- Email Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('email', 'Email:') !!}
-    {!! Form::text('email', null, ['class' => 'form-control']) !!}
-</div>
+    <!-- Email Field -->
+    <div class="form-group">
+        {!! Form::label('email', 'Email:') !!}
+        {!! Form::text('email', null, ['class' => 'form-control', 'required' => 'required']) !!}
+    </div>
 
-<!-- Password Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('password', 'Contraseña:') !!}
-    {{-- {!! Form::password('password',['class' => 'form-control', 'placeholder' => 'Password', 'type' => 'password']) !!} --}}
-    {!! Form::password('password', ['class' => 'form-control']) !!}
-</div>
+    <!-- Password Field -->
+    <div class="form-group">
+        {!! Form::label('password', 'Contraseña:') !!}
+        {!! Form::password('password', ['class' => 'form-control', 'required' => 'required']) !!}
+    </div>
 
-<!-- Submit Field -->
-<div class="form-group col-sm-12">
-    {!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
-    <a href="{!! route('supervisores.index') !!}" class="btn btn-default">Cancelar</a>
+    <!-- Supervisor Field -->
+    <div class="form-group">
+        {!! Form::label('role_name', 'Elija un rol:') !!}
+        {!! Form::select('role_name', ['Supervisor 1'=>'Supervisor 1', 'Supervisor 2'=>'Supervisor 2'], null, ['class' => 'form-control', 'required' => 'required']) !!}
+    </div>
+
+    <!-- Submit Field -->
+    <div class="form-group">
+        {!! Form::submit('Guardar', ['class' => 'btn btn-primary']) !!}
+        <a href="{!! route('supervisores.index') !!}" class="btn btn-default">Cancelar</a>
+    </div>
 </div>
-    
