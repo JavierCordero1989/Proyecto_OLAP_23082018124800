fa1e7322ab92290d1966b8a3233e9be8eafe9101
JavierCordero1989 <javicordero20022016@gmail.com>
2018-10-22 12:38:52 -0600

    archivo calendar-routes
    Se agregan las rutas para que el agregar cita de las entrevistas funcione para todos.

--- config/calendar-routes.php
+++ config/calendar-routes.php
@@ -1,6 +1,10 @@
 <?php
     return [
         '0a' => 'supervisores.index',
-        '0b' => 'asignar-encuestas.realizar-entrevista'
+        '0b' => 'asignar-encuestas.realizar-entrevista',
+        '1a' => 'supervisor2.lista-de-supervisores',
+        '1b' => 'supervisor2.realizar-entrevista',
+        '2a' => 'home',
+        '2b' => 'encuestador.realizar-entrevista'
     ];
 ?>
