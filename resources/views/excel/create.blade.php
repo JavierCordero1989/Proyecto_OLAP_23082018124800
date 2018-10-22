649e6960fac41a2a8c9a479c645bc07d67e64609
JavierCordero1989 <javicordero20022016@gmail.com>
2018-10-22 14:55:51 -0600

    vista excel/create
    Se elimina el código ajax

--- resources/views/excel/create.blade.php
+++ resources/views/excel/create.blade.php
@@ -49,39 +49,8 @@
     <!-- Script para obtener los datos del formulario y realizar la peticion AJAX -->
     <script>
         function eventoModalFormulario() {
-            // $('#modalLoadingLetters').modal('show');
+            $('#modalLoadingLetters').modal('show');
             return true;
         }
-
-        // $(function(){
-        //     $('#form_importar_excel').on('submit', function(e) {
-        //         e.preventDefault();
-        //         var f = $(this);
-
-        //         //Se obtiene el formulario
-        //         var formData = new FormData(document.getElementById('form_importar_excel'));
-
-        //         $.ajax({
-        //             url: document.getElementById('form_importar_excel').action,
-        //             type: 'post',
-        //             dataType: 'html',
-        //             data: formData,
-        //             cache: false,
-        //             contentType: false,
-        //             processData: false,
-        //             beforeSend: function() {
-        //                 $('#modalLoadingLetters').modal('show');
-        //             },
-        //             success: function(result) {
-        //                 $('#modalLoadingLetters').modal('hide');
-        //                 $('#success-text').html('Se han guardado correctamente todos los registros');
-        //                 $('#modal-success').modal('show');
-        //                 console.log("Success: ", result);
-        //             }
-        //         });
-        //     });
-        // });
-
-        // eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('$(0(){$(\'#3\').l(\'r\',0(e){e.v();b f=$(n);b 9=s t(7.5(\'3\'));$.d({g:7.5(\'3\').h,i:\'j\',k:\'6\',m:9,H:4,o:4,p:4,q:0(){$(\'#8\').1(\'a\')},2:0(c){$(\'#8\').1(\'w\');$(\'#2-x\').6(\'y z A B C D E\');$(\'#1-2\').1(\'a\');F.G("u: ",c)}})})});',44,44,'function|modal|success|form_importar_excel|false|getElementById|html|document|modalLoadingLetters|formData|show|var|result|ajax|||url|action|type|post|dataType|on|data|this|contentType|processData|beforeSend|submit|new|FormData|Success|preventDefault|hide|text|Se|han|guardado|correctamente|todos|los|registros|console|log|cache'.split('|'),0,{}))
     </script>
 @endsection
