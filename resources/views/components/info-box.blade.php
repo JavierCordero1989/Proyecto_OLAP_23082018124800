835847db0e1bd9668550f4e6be1bdee356e0bc73
JavierCordero1989 <javicordero20022016@gmail.com>
2018-10-23 12:43:51 -0600

    Componente info-box
    Se agrega un componente para las cajas de estado para los reportes

---
+++ resources/views/components/info-box.blade.php
@@ -0,0 +1,17 @@
+<div class="col-md-3 col-sm-6 col-xs-12">
+    <div class="info-box">
+        {{-- <span class="info-box-icon bg-yellow"> --}}
+        <span class="info-box-icon {!! $color_class !!}">
+            {{-- <i class="ion ion-ios-people-outline"></i> --}}
+            <i class="{!! $icon !!}"></i>
+        </span>
+
+        <div class="info-box-content">
+            <span class="info-box-text">{!! $info_text !!}</span>
+            <span class="info-box-number">{!! $data !!}</span>
+            <span class="info-box-text">{!! $percent !!}</span>
+        </div>
+        <!-- /.info-box-content -->
+    </div>
+    <!-- /.info-box -->
+</div>
