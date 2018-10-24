e1bd43f9a7d5280b3769f252396ecf0ebebee2eb
JavierCordero1989 <javicordero20022016@gmail.com>
2018-10-24 15:40:19 -0600

    componente table
    Se crea un nuevo componente, para crear tablas de forma dinámica y mediante un solo archivo

---
+++ resources/views/components/table.blade.php
@@ -0,0 +1,14 @@
+<table class="table table-hover">
+    <thead>
+        @foreach ($encabezados as $titulo)
+            <th>{!! $titulo !!}</th>
+        @endforeach
+    </thead>
+
+    <tbody>
+        {!! $cuerpo_tabla !!}
+    </tbody>
+
+</table>
+
+{!! $paginacion !!}
