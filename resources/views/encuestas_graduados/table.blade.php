253402e50648aadb07cbfacd0a9de17aad8a17e5
JavierCordero1989 <javicordero20022016@gmail.com>
2018-10-24 15:41:00 -0600

    vista encuestas_graduados/table
    Se agrega el componente a la vista

--- resources/views/encuestas_graduados/table.blade.php
+++ resources/views/encuestas_graduados/table.blade.php
@@ -2,43 +2,33 @@
     @include('layouts.datatables_css')
 @endsection --}}
 
-<table class="table table-hover">
-    <thead>
-        <th>Identificacion</th>
-        <th>Nombre</th>
-        <th>Sexo</th>
-        <th>Carrera</th>
-        <th>Universidad</th>
-        <th>Grado</th>
-        <th>Disciplina</th>
-        <th>Área</th>
-        <th>Agrupación</th>
-        <th>Sector</th>
-        <th>Tipo de caso</th>
-        {{-- <th>Opciones</th> --}}
-    </thead>
-    <tbody>
-    @foreach($encuestas as $encuesta)
-        <tr>
-            <td>
-                <a href="#modal-ver-detalles-de-entrevista-{{$encuesta->id}}" data-toggle="modal">{!! $encuesta->identificacion_graduado !!}</a>
-                @include('modals.modal_ver_detalles_de_entrevista')
-            </td>
-            <td>{!! $encuesta->nombre_completo !!}</td>
-            <td>{!! $encuesta->sexo !!}</td>
-            <td>{!! $encuesta->carrera->nombre !!}</td>
-            <td>{!! $encuesta->universidad->nombre !!}</td>
-            <td>{!! $encuesta->grado->nombre !!}</td>
-            <td>{!! $encuesta->disciplina->descriptivo !!}</td>
-            <td>{!! $encuesta->area->descriptivo !!}</td>
-            <td>{!! $encuesta->agrupacion->nombre !!}</td>
-            <td>{!! $encuesta->sector->nombre !!}</td>
-            <td>{!! $encuesta->tipo_de_caso !!}</td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
-{!! $encuestas->render() !!}
+@component('components.table')
+    @slot('encabezados', ['Identificación', 'Nombre', 'Sexo', 'Carrera', 'Universidad', 'Grado', 'Disciplina', 'Área', 'Agrupación', 'Sector', 'Tipo de caso'])
+    
+    @slot('cuerpo_tabla')
+        @foreach($encuestas as $encuesta)
+            <tr>
+                <td>
+                    <a href="#modal-ver-detalles-de-entrevista-{{$encuesta->id}}" data-toggle="modal">{!! $encuesta->identificacion_graduado !!}</a>
+                    @include('modals.modal_ver_detalles_de_entrevista')
+                </td>
+                <td>{!! $encuesta->nombre_completo !!}</td>
+                <td>{!! $encuesta->sexo == 'M' ? 'Hombre' : ($encuesta->sexo == 'F' ? 'Mujer' : 'ND') !!}</td>
+                <td>{!! $encuesta->carrera->nombre !!}</td>
+                <td>{!! $encuesta->universidad->nombre !!}</td>
+                <td>{!! $encuesta->grado->nombre !!}</td>
+                <td>{!! $encuesta->disciplina->descriptivo !!}</td>
+                <td>{!! $encuesta->area->descriptivo !!}</td>
+                <td>{!! $encuesta->agrupacion->nombre !!}</td>
+                <td>{!! $encuesta->sector->nombre !!}</td>
+                <td>{!! $encuesta->tipo_de_caso !!}</td>
+            </tr>
+        @endforeach
+    @endslot
+
+    @slot('paginacion', $encuestas->render())
+@endcomponent
+
 {{-- @section('scripts')
     @include('layouts.datatables_js')
 @endsection --}}
