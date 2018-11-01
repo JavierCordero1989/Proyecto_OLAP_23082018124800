cd6efede64b5cb08acc683f0efefcad5c6b69a9a
José Javier Cordero León <javicordero20022016@gmail.com>
2018-11-01 14:04:17 -0600

    vista auth/success-message
    Se crea una vista con un mensaje de respuesta al realizar la solicitud de cambio de contraseña

---
+++ resources/views/auth/success-message.blade.php
@@ -0,0 +1,16 @@
+<div class="success-message">
+    <svg viewBox="0 0 76 76" class="success-message__icon icon-checkmark">
+         <circle cx="38" cy="38" r="36"/>
+         <path fill="none" stroke="#FFFFFF" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M17.7,40.9l10.9,10.9l28.7-28.7"/>
+    </svg>
+     <h1 class="success-message__title">Solicitud enviada</h1>
+     <div class="success-message__content">
+       <h4><b>Le responderemos a la brevedad posible</b></h4>
+     </div>
+    <a href="{!! url('/login') !!}" class="btn btn-success success-button__content">
+       <i class="glyphicon glyphicon-log-out"></i>
+       <b>Regresar</b>
+    </a>
+ </div>
+ 
+ <!-- Este código fue copiado de https://codepen.io/souporserious/pen/MwmXdg y adaptado para cumplir una necesidad específica -->
