2aaaf0aec9414b307179f15e94e304ed02d75c90
José Javier Cordero León <javicordero20022016@gmail.com>
2018-11-04 23:00:10 -0600

    Componentes
    Mensaje de error y de exito agregados, para mejor visualizacion de las alertas

---
+++ resources/views/components/error-message.blade.php
@@ -0,0 +1,6 @@
+@if (isset($error))
+    <div class="error">
+        <i class="glyphicon glyphicon-exclamation-sign"></i>
+        {!! $error !!}
+    </div>
+@endif
---
+++ resources/views/components/success-message.blade.php
@@ -0,0 +1,6 @@
+@if (isset($success))
+    <div class="success">
+        <i class="glyphicon glyphicon-ok-sign"></i>
+        {!! $success !!}
+    </div>
+@endif
