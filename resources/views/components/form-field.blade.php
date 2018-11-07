2a14d8fb9e194cc048301ece245e44744c396e00
JavierCordero1989 <javicordero20022016@gmail.com>
2018-11-07 15:52:08 -0600

    Componente form-field
    Componente para campos de formulario

---
+++ resources/views/components/form-field.blade.php
@@ -0,0 +1,10 @@
+<div class="form-group {!! $otras_clases !!}">
+    <label class="control-label {!! $tam_label !!}" for="{!! $for_label !!}">{!! $label_text !!}:</label>
+    <div class="{!! $tam_box !!} inputGroupContainer">
+        <div class="input-group">
+            <span class="input-group-addon"><i class="{!! $field_icon !!}"></i></span>
+            {!! $form_field !!}
+            <input type="text" class="form-control" name="identificacion_graduado" id="identificacion_graduado">
+        </div>
+    </div>
+</div>
