c13d163f02055788e3bbecb1ccb3d71b1ddd9dcf
JavierCordero1989 <javicordero20022016@gmail.com>
2018-11-07 14:14:01 -0600

    Vista encuestas_graduados/agregar-contacto.blade.php
    Se agrega validación al formulario para agregar un nuevo contacto.

--- resources/views/encuestas_graduados/agregar-contacto.blade.php
+++ resources/views/encuestas_graduados/agregar-contacto.blade.php
@@ -2,6 +2,10 @@
 
 @section('title', "Crear Encuestador")
 
+@section('css')
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.0/css/bootstrapValidator.min.css">
+@endsection
+
 @section('content')
     <section class="content-header">
         <h1>
@@ -14,7 +18,7 @@
 
             <div class="box-body">
                 <div class="row">
-                    {!! Form::open(['route' => ['encuestas-graduados.guardar-contacto', $id_encuesta]]) !!}
+                    {!! Form::open(['route' => ['encuestas-graduados.guardar-contacto', $id_encuesta], 'id'=>'form-nuevo-contacto']) !!}
 
                         <!-- Campo para la identificacion de la referencia -->
                         <div class="form-group col-sm-6">
@@ -26,6 +30,12 @@
                         <div class="form-group col-sm-6">
                             {!! Form::label('nombre_referencia', 'Nombre de la referencia:') !!}
                             {!! Form::text('nombre_referencia', null, ['class' => 'form-control']) !!}
+                        </div>
+
+                        <!-- Campo para el parentezco -->
+                        <div class="form-group col-sm-6">
+                            {!! Form::label('parentezco', 'Parentezco con el graduado:') !!}
+                            {!! Form::text('parentezco', null, ['class' => 'form-control']) !!}
                         </div>
 
                         <!-- Campo para el nombre de la referencia -->
@@ -51,4 +61,84 @@
             </div>
         </div>
     </div>
+@endsection
+
+@section('scripts')
+    <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-validator/0.4.5/js/bootstrapvalidator.min.js"></script>
+    <script>
+         $(function() {
+            $('#form-nuevo-contacto').bootstrapValidator({
+                feedbackIcons: {
+                    valid: 'glyphicon glyphicon-ok',
+                    invalid: 'glyphicon glyphicon-remove',
+                    validating: 'glyphicon glyphicon-refresh'
+                }, 
+                fields: {
+                    identificacion_referencia: {
+                        validators: {
+
+                        }
+                    },
+                    nombre_referencia: {
+                        validators: {
+                            callback:{
+                                message: 'Al menos el nombre del contacto es requerido.',
+                                callback: function(value, validator, $field) {
+                                    let parentezco_contacto = $('#parentezco').val();
+                                    console.log('Nombre: '+parentezco_contacto);
+
+                                    return (value != '' || parentezco_contacto != '');
+                                }
+                            }
+                        }
+                    },
+                    parentezco: {
+                        validators: {
+                            callback:{
+                                message: 'Al menos el parentezco con el graduado es requerido.',
+                                callback: function(value, validator, $field) {
+                                    let nombre_contacto = $('#nombre_referencia').val();
+                                    console.log('Parentezco: ' + nombre_contacto);
+
+                                    return (value != '' || nombre_contacto != '');
+                                }
+                            }
+                        }
+                    },
+                    informacion_contacto: {
+                        validators: {
+                            notEmpty:{
+                                message: 'El contacto es un dato requerido.'
+                            }
+                        }
+                    },
+                    observacion_contacto: {
+                        validators: {
+                            stringLength:{
+                                min: 0,
+                                max: 200,
+                                message: 'La observación no puede contener más de 200 caractéres.'
+                            }
+                        }
+                    },
+                }
+            })
+            .on('success.form.bv', function(e){
+                //$('#success_message').slideDown({opacity: "show"}, "slow")
+                $('#form-nuevo-contacto').data('bootstrapValidator').resetForm();
+                
+                //Previene el submit
+                e.preventDefault();
+                
+                // Obtiene la instancia del formulario
+                let $formulario = $(e.target);
+                
+                //Obtiene la instancia del validador de bootstrap
+                let bv = $formulario.data('bootstrapValidator');
+                
+                // Hacer algo con el submit
+                this.submit();
+            });
+        });
+    </script>
 @endsection
