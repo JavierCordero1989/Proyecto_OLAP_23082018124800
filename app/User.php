03cce0b62a57529d1fa10a4db41b7e51dbd0b8c1
José Javier Cordero León <javicordero20022016@gmail.com>
2018-11-12 13:32:29 -0600

    Modelo User
    Se agregan las variables extension y mobile

--- app/User.php
+++ app/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'user_code', 'name', 'email', 'password',
+        'user_code', 'extension', 'mobile', 'name', 'email', 'password',
     ];
 
     /**
