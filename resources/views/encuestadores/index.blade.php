036ea241ae88b3319abd64f17ab792d6cdb2657a
José Javier Cordero León <javicordero20022016@gmail.com>
2018-11-12 22:15:58 -0600

    vista de lista de encuestadores
    Se elimina el foreach creado con blade y php, y se sustituye por un v-fro de VUE.js, para agregar campos de busqueda para filtrar los encuestadores

--- resources/views/encuestadores/index.blade.php
+++ resources/views/encuestadores/index.blade.php
@@ -3,26 +3,126 @@
 @section('title', 'Encuestadores') 
 
 @section('content')
-    <section class="content-header">
-        <h1 class="pull-left">Encuestadores</h1>
-        <h1 class="pull-right">
-           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('encuestadores.create') !!}">Agregar nuevo</a>
-        </h1>
-    </section>
-    <div class="content">
-        <div class="clearfix"></div>
+    <div id="app">
+        <section class="content-header">
+            <div class="col-xs-12">
+                <h1 class="pull-left">
+                    <a href="#" class="btn btn-default pull-left" v-on:click="clearInputs">Limpiar campos</a>
+                </h1>
+                <h1 class="pull-right">
+                    <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('encuestadores.create') !!}">Agregar nuevo</a>
+                </h1>
+            </div>
 
-        @include('flash::message')
+            <div class="col-xs-12">
+                <div class="col-xs-12 col-sm-12 col-md-4">
+                    <div class="form-group">
+                        <label for="search_user_code" class="control-label">Código: @{{user_code}}</label>
+                        <div class="input-group">
+                            <input type="search" class="form-control" v-model="user_code">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span>
+                        </div>
+                    </div>
+                </div>
 
-        <div class="clearfix"></div>
-        <div class="box-header">
-            <div class="box-body">
-                @include('encuestadores.table')
+                <div class="col-xs-12 col-sm-12 col-md-4">
+                    <div class="form-group">
+                        <label for="search_name_code" class="control-label">Nombre: @{{name}}</label>
+                        <div class="input-group">
+                            <input type="search" class="form-control" v-model="name">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xs-12 col-sm-12 col-md-4">
+                    <div class="form-group">
+                        <label for="search_email" class="control-label">Correo electrónico: @{{email}}</label>
+                        <div class="input-group">
+                            <input type="search" class="form-control" v-model="email">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="content">
+            <div class="clearfix"></div>
+
+            @include('flash::message')
+
+            <div class="clearfix"></div>
+            <div class="box-header">
+                <div class="box-body">
+                    @include('encuestadores.table')
+                </div>
             </div>
         </div>
-        {{-- <div class="text-center">
-        
-        </div> --}}
     </div>
 @endsection
 
+@section('scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.17/vue.min.js"></script>
+    <script>
+        let lista = <?php echo json_encode($lista_encuestadores); ?>
+
+        new Vue({
+            el: '#app',
+            data: {
+                user_code: '',
+                name: '',
+                email: '',
+                conected_user: '{{Auth::user()->id}}',
+                lista_encuestadores: lista
+            },
+            methods: {
+                getDeleteRoute: function(id) {
+                    let route = '{{ route("encuestadores.destroy", ":id") }}'
+                    route = route.replace(":id", id)
+                    return route
+                },
+                getEditRoute: function(id) {
+                    let route = '{{ route("encuestadores.edit", ":id") }}'
+                    route = route.replace(":id", id)
+                    return route
+                },
+                getAssignedInterviewsRoute: function(id) {
+                    let route = '{{ route("asignar-encuestas.lista-encuestas-asignadas", ":id") }}'
+                    route = route.replace(":id", id)
+                    return route
+                },
+                assignInterviews: function(id) {
+                    let route = '{{ route("asignar-encuestas.asignar", [":id_user", ":id"]) }}'
+                    route = route.replace(":id_user", this.conected_user)
+                    route = route.replace(":id", id)
+                    return route
+                },
+                clearInputs: function(event) {
+                    console.log('limpiando campos...')
+                    this.user_code = ''
+                    this.name = ''
+                    this.email= ''
+                }
+            },
+            computed: {
+                listaFiltro() {
+                    let filtro = this.lista_encuestadores
+                    
+                    if(this.user_code != '') {
+                        filtro = filtro.filter(element=>element.user_code.toLowerCase().includes(this.user_code.toLowerCase()))
+                    }
+
+                    if(this.name != '') {
+                        filtro = filtro.filter(element=>element.name.toLowerCase().includes(this.name.toLowerCase()))
+                    }
+
+                    if(this.email != '') {
+                        filtro = filtro.filter(element=>element.email.toLowerCase().includes(this.email.toLowerCase()))
+                    }
+
+                    return filtro
+                }
+            }
+        })
+    </script>
+@endsection
--- resources/views/encuestadores/table.blade.php
+++ resources/views/encuestadores/table.blade.php
@@ -1,108 +1,126 @@
 <section class="content">
     <div class="row">
-        @foreach($lista_encuestadores as $encuestador)
-            <div class="col-md-6">
-                <div class="box box-primary collapsed-box" >
-                    <!-- Encabezado del cuadro -->
-                    <div class="box-header with-border">
-                        <h3 class="box-title">
-                            <span>{!! $encuestador->user_code !!}</span> - {!! $encuestador->name !!}
-                        </h3>
-      
-                        <!-- Botones de la parte superior derecha -->
-                        <div class="box-tools pull-right">
-                            @if(Auth::user()->hasRole(['Super Admin', 'Supervisor 1']))
-                                {!! Form::open(['route' => ['encuestadores.destroy', $encuestador->id], 'method' => 'delete']) !!}
-                            @endif    
-                                <div class='btn-group'>
+        <div class="col-xs-12 col-sm-12 col-md-6" v-for="encuestador in listaFiltro">
+            <div class="box box-primary collapsed-box">
+                <div class="box-header with-border">
+                    <h3 class="box-title">
+                        <span>@{{encuestador.user_code}}</span> - @{{encuestador.name}}
+                    </h3>
+                    <!-- Botones de la parte superior derecha -->
+                    <div class="box-tools pull-right">
+                        @if(Auth::user()->hasRole(['Super Admin', 'Supervisor 1']))
+                            <form :action="getDeleteRoute(encuestador.id)" method="delete">
+                            {{ csrf_field() }}
+                            <input type="hidden" name="_method" value="DELETE">
+                        @endif  
+                            
+                        <div class="btn-group">
+                            <a :href="'#modal-'+encuestador.id" data-toggle="modal" class='btn btn-default btn-xs'>
+                                <i class="glyphicon glyphicon-eye-open"></i>
+                            </a>
 
-                                    <!-- Boton para ver los datos del encuestador -->
-                                    {{-- <a href="{!! route('encuestadores.show', [$encuestador->id]) !!}" class='btn btn-default btn-xs'>
-                                        <i class="glyphicon glyphicon-eye-open"></i>
-                                    </a> --}}
-                                    
-                                    {{-- <a href="#modal-{!! $encuestador->id !!}" data-toggle="modal" class='btn btn-default btn-xs'>
-                                        <i class="glyphicon glyphicon-eye-open"></i>
-                                    </a> --}}
+                            <!-- modal de la info de usuario -->
+                            <div class="modal fade" :id="'modal-'+encuestador.id">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                            <h4 class="modal-title" v-text="encuestador.name"></h4>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div class="row" style="padding-left: 20px">
+                                                <div class="col-xs-12">
+                                                    <!-- User code Field -->
+                                                    <div class="form-group">
+                                                        {!! Form::label('user_code', 'Código:') !!}
+                                                        <p v-text="encuestador.user_code"></p>
+                                                    </div>
+                                                    
+                                                    <!-- Extension Field -->
+                                                    <div class="form-group">
+                                                        {!! Form::label('extension', 'Extensión:') !!}
+                                                        <p v-text="encuestador.extension"></p>
+                                                    </div>
 
-                                    @component('components.info-encuestador')
-                                        @slot('id_modal', 'modal-'.$encuestador->id)
-                                        @slot('nombre_encuestador', $encuestador->name)
-                                        @slot('codigo_usuario', $encuestador->user_code)
-                                        @slot('extension', $encuestador->extension)
-                                        @slot('mobile', $encuestador->mobile)
-                                        @slot('email', $encuestador->email)
-                                        @slot('created_at', $encuestador->created_at)
-                                        @slot('updated_at', $encuestador->updated_at)
-                                    @endcomponent
+                                                    <!-- Mobile Number -->
+                                                    <div class="form-group">
+                                                        {!! Form::label('mobile', 'Celular:') !!}
+                                                        <p v-text="encuestador.mobile"></p>
+                                                    </div>
 
-                                    {{-- @include('encuestadores.modal-info-encuestador') --}}
+                                                    <!-- Nombre Field -->
+                                                    <div class="form-group">
+                                                        {!! Form::label('email', 'Email:') !!}
+                                                        <p v-text="encuestador.email"></p>
+                                                    </div>
 
-                                    <!-- Boton para editar los datos del encuestador -->
-                                    {{-- @if (Auth::user()->hasRole('Administrador')) --}}
-                                        <a href="{!! route('encuestadores.edit', [$encuestador->id]) !!}" class='btn btn-default btn-xs'>
-                                            <i class="glyphicon glyphicon-edit"></i>
-                                        </a>
-                    
-                                        @if(Auth::user()->hasRole(['Super Admin', 'Supervisor 1']))
-                                            <!-- Boton para eliminar los datos del encuestador -->
-                                            {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs','onclick' => "return confirm('¿Está seguro de querer eliminar?')", 'data-toggle' => 'modal', 'data-target' => '#modal-danger']) !!}
-                                        @endif
-                                    {{-- @endif --}}
-
-                                    <!-- Boton para minimizar/maximiar cada cuadro -->
-                                    <button type="button" class="btn btn-info btn-xs" data-widget="collapse">
-                                        <i class="fas fa-plus"></i>
-                                    </button>
-      
+                                                    <div class="form-group">
+                                                        {!! Form::label('created_at', 'Creado el: ') !!}
+                                                        <p v-text="encuestador.created_at"></p>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        {!! Form::label('updated_at', 'Modificado el: ') !!}
+                                                        <p v-text="encuestador.updated_at"></p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
+                            </div>
+                            <!-- termina el modal -->
+
+                            <a :href="getEditRoute(encuestador.id)" class='btn btn-default btn-xs'>
+                                <i class="glyphicon glyphicon-edit"></i>
+                            </a>
+        
                             @if(Auth::user()->hasRole(['Super Admin', 'Supervisor 1']))
-                                {!! Form::close() !!}
+                                <!-- Boton para eliminar los datos del encuestador -->
+                                {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs','onclick' => "return confirm('¿Está seguro de querer eliminar?')"]) !!}
                             @endif
-                        </div>
-                    </div>
 
-                    <!-- Imagen del cuadro -->
-                    <div class="box-body">
-                        <div class="col-md-12">
-                            <img class="card-img-top" data-src="{{ config('global.imagen_tarjetas') }}" alt="logo de OLAP" style="height: 100%; width: 100%; display: block;" src="{{ asset(config('global.imagen_tarjetas')) }}" data-holder-rendered="true">
-                        </div>
-                    </div>
-
-                    <!-- Botones del cuadro, parte inferior -->
-                    <div class="box-body">
-                        <div class="row">
-                            <div class="col-xs-5 col-xs-offset-1">
-                                <a href="{{ route('asignar-encuestas.asignar', [Auth::user()->id, $encuestador->id ]) }}" class="btn btn-primary btn-sm col-sm-12">
-                                    <i class="fa fa-plus-square"></i> Asignar Encuestas
-                                </a>
-                            </div>
-
-                            <div class="col-xs-5">
-                                <a href="{{ route('asignar-encuestas.lista-encuestas-asignadas', [$encuestador->id]) }}" class="btn btn-primary btn-sm col-sm-12">
-                                    <i class="far fa-eye"></i> Encuestas asignadas
-                                </a>
-                            </div>
+                            <!-- Boton para minimizar/maximiar cada cuadro -->
+                            <button type="button" class="btn btn-info btn-xs" data-widget="collapse">
+                                <i class="fas fa-plus"></i>
+                            </button>
 
                         </div>
-                        
-                        {{-- <div class="row" style="margin-top: 15px;">
-                            <div class="col-xs-5 col-xs-offset-1">
-                                <a href="#" class="btn btn-primary btn-sm col-sm-12">
-                                    <i class="fa fa-eyedropper"></i> Botón 2
-                                </a>
-                            </div>
 
-                            <div class="col-xs-5">
-                                <a href="{{ route('graficos.graficos-por-encuestador', [$encuestador->id]) }}" class="btn btn-primary btn-sm col-sm-12">
-                                    <i class="fa fa-area-chart"></i> Ver estadísticas
-                                </a>
-                            </div>
-                        </div> --}}
+                        @if(Auth::user()->hasRole(['Super Admin', 'Supervisor 1']))
+                            </form>
+                        @endif
                     </div>
                 </div>
+
+                <!-- Cuerpo de cada caja -->
+                <div class="box-body">
+                    <div class="col-md-12">
+                        <img class="card-img-top" data-src="{{ config('global.imagen_tarjetas') }}" alt="logo de OLAP" style="height: 100%; width: 100%; display: block;" src="{{ asset(config('global.imagen_tarjetas')) }}" data-holder-rendered="true">
+                    </div>
+                </div>
+
+                <!-- botones para acceder a las encuestas -->
+                <div class="box-body">
+                    <div class="row">
+                        <div class="col-xs-5 col-xs-offset-1">
+                            <a :href="assignInterviews(encuestador.id)" class="btn btn-primary btn-sm col-sm-12">
+                                <i class="fa fa-plus-square"></i> Asignar Encuestas
+                            </a>
+                        </div>
+
+                        <div class="col-xs-5">
+                            <a :href="getAssignedInterviewsRoute(encuestador.id)" class="btn btn-primary btn-sm col-sm-12">
+                                <i class="far fa-eye"></i> Encuestas asignadas
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <!-- Fin del .box-body -->
+
             </div>
-        @endforeach
+        </div>
     </div>
 </section>
       
