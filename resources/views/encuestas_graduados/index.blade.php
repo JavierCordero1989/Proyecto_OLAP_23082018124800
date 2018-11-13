df822336c358cdbcdaee923e6d03ab867427eeb6
JavierCordero1989 <javicordero20022016@gmail.com>
2018-11-13 14:22:18 -0600

    index encuestas
    se agrega VUE.js

--- resources/views/encuestas_graduados/index.blade.php
+++ resources/views/encuestas_graduados/index.blade.php
@@ -3,26 +3,49 @@
 @section('title', 'Encuestadores') 
 
 @section('content')
-    <section class="content-header">
-        <h1 class="pull-left">Lista de encuestas</h1>
-        <h1 class="pull-right">
-           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('encuestas-graduados.create') !!}">Agregar nueva</a>
-        </h1>
-    </section>
-    <div class="content">
-        <div class="clearfix"></div>
-
-        @include('flash::message')
-
-        <div class="clearfix"></div>
-        <div class="box-header">
-            <div class="box-body table-responsive">
-                @include('encuestas_graduados.table')
+    <div id="app">
+        <section class="content-header">
+            <h1 class="pull-left">Lista de encuestas</h1>
+            <h1 class="pull-right">
+               <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('encuestas-graduados.create') !!}">Agregar nueva</a>
+            </h1>
+        </section>
+        <div class="content">
+            <div class="clearfix"></div>
+    
+            @include('flash::message')
+    
+            <div class="clearfix"></div>
+            <div class="box-header">
+                <div class="box-body table-responsive">
+                    @include('encuestas_graduados.New_table')
+                </div>
             </div>
+            {{-- <div class="text-center">
+            
+            </div> --}}
         </div>
-        {{-- <div class="text-center">
-        
-        </div> --}}
     </div>
+@endsection
+
+@section('scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.5.17/vue.min.js"></script>
+    <script>
+        let lista = <?php echo json_encode($encuestas); ?>
+
+        new Vue({
+            el: '#app',
+            data: {
+                lista_encuestas: lista
+            },
+            methods: {
+                eventoEliminar: function(evento) {
+                    if(!confirm('¿Desea eliminar el registro de todas formas?')) {
+                        evento.preventDefault()
+                    }
+                }
+            }
+        })
+    </script>
 @endsection
 
