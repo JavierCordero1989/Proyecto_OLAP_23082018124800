f378172eb92a8c32bf0b69efd0ee8ed2bda2d043
José Javier Cordero León <javicordero20022016@gmail.com>
2018-11-14 23:01:25 -0600

    Archivo options
    Se agrega un archivo global con las opciones para ver los tipos en sexos y casos

---
+++ config/options.php
@@ -0,0 +1,16 @@
+<?php
+    return [
+        'sex_types' => [
+            ''=>'- - - Seleccione - - -',
+            'M'=>'Hombre',
+            'F'=>'Mujer',
+            'SC'=>'Sin Clasificar'
+        ],
+        'case_types' => [
+            '' => '- - - Seleccione - - -',
+            'MUESTRA'=>'Muestra',
+            'CENSO'=>'Censo',
+            'SUSTITUCION'=>'Sustitucion'
+        ]
+    ];
+?>
