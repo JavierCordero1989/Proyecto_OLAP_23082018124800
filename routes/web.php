0b5ff2710ad7cd9c11056a0be2d31b25c6939bbb
JavierCordero1989 <javicordero20022016@gmail.com>
2018-11-22 15:37:39 -0600

    Archivo de rutas
    Se agrega la ruta para obtener los datos de los reportes mediante axios

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home/reporte-general', 'HomeController@reportes_generales')->name('home.reporte-general')->middleware('auth');
 
 //Rutas para los roles
 Route::group(['middleware'=>['auth']], function() {
