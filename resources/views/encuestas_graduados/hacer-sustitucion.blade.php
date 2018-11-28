3a9547c11c9b0901f46bb6456433fcdfe50d7f0d
JavierCordero1989 <javicordero20022016@gmail.com>
2018-11-28 11:50:00 -0600

    Vista encuestas_graduados/hacer-sustitucion
    Se agrega la vista para realizar sustituciones.

---
+++ resources/views/encuestas_graduados/hacer-sustitucion.blade.php
@@ -0,0 +1,100 @@
+@extends('layouts.app')
+
+@section('title', 'Sustitución')
+
+@section('content')
+    <div class="content">
+        <!-- Cuadro para notificaciones -->
+        <div class="clearfix"></div>
+
+        @include('flash::message')
+
+        <div class="clearfix"></div>
+
+        <div class="box box-primary">
+            <div class="box-body with-border">
+                {!! Form::open(['route'=>'encuestas-graduados.hacer-sustitucion-post', 'class'=>'']) !!}
+                    <div class="form-group">
+                        {!! Form::label('token_entrevista', 'Ingrese el token de la entrevista a sustituir:') !!}
+                        {!! Form::text('token_entrevista', null, ['class'=>'form-control']) !!}
+                        <span id="mensaje-alerta"></span>
+                    </div>
+
+                    <div class="form-group">
+                        {!! Form::button('<i class="fas fa-search"></i> Buscar...', [
+                            'class'=>'btn btn-default',
+                            'onclick'=>'buscarEncuesta()'
+                        ]) !!}
+
+                        {!! Form::button('<i class="fas fa-exchange-alt"></i> Sustituir', [
+                            'class'=>'btn btn-primary',
+                            'type'=>'submit',
+                            'onclick'=>'return validarTokenEnBlanco();',
+                            'disabled'=>'true'
+                        ]) !!}
+
+                        <a href="{!! url('home') !!}" class="btn btn-default">
+                            <i class="fas fa-arrow-left"></i>
+                            Cancelar
+                        </a>
+                    </div>
+                {!! Form::close() !!}
+            </div>
+        </div>
+
+        <div class="box box-primary">
+            <div class="box-body with-border">
+                <p>Hola mundo</p>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('scripts')
+    <script>
+        let token = $('#token_entrevista')
+
+        function validarTokenEnBlanco() {
+            if(token.val() == '') {
+                alert('Debe ingresar un token para realizar la búsqueda.')
+                return false;
+            }
+            return true;
+        }
+
+        function buscarEncuesta() {
+            if(token.val() == '') {
+                alert('Debe ingresar un token para realizar la búsqueda.');
+            }
+            else {
+                let encuesta = null
+                let url = '{{ route("encuestas-graduados.buscar-encuesta") }}';
+
+                axios.get(url, {
+                    params: {
+                        token: token.val()
+                    }
+                }).then(response=>{
+                    encuesta = response.data
+
+                    if(encuesta == 'encuesta no encontrada') {
+                        mostrarMensajeDeError()
+                    }
+                    else {
+                        cargarEncuestaEncontrada(encuesta)
+                    }
+                })
+            }
+        }
+
+        function cargarEncuestaEncontrada(encuesta) {
+            // Carga la encuesta
+            console.log('DATOS ENCONTRADOS: ' + encuesta);
+        }
+
+        function mostrarMensajeDeError() {
+            //No carga nada
+            console.log('NO SE ENCONTRARON DATOS.');
+        }
+    </script>
+@endsection
